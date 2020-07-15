c3d5a9203900d73a6b0b9f48bc7e886d0b07e9b3
Tom Hole <git@stirtingale.com>
2020-07-15 14:40:17 +0200

    Requirements

--- functions.php
+++ functions.php
@@ -5,6 +5,61 @@ add_theme_support( 'menus' );
 
 /* ========================================================================================================================
 
+Register Navigation 
+
+======================================================================================================================== */
+
+register_nav_menus(array('primary' => 'Primary Navigation'));
+
+/* ========================================================================================================================
+
+Plugin Requirements 
+
+======================================================================================================================== */
+
+require_once( 'tgm/class-tgm-plugin-activation.php' );
+
+add_action( 'tgmpa_register', 'stirtingale_required_register_required_plugins' );
+
+function stirtingale_required_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		array(
+			'name'        => 'Advanced Custom Fields PRO',
+			'slug'        => 'advanced-custom-fields-pro',
+			'is_callable' => 'wpseo_init',
+		),
+		array(
+			'name'        => 'WP API Menus',
+			'slug'        => 'wp-api-menus'
+		),
+		// array(
+		// 	'name'        => 'Intagrate',
+		// 	'slug'        => 'instagrate-pro'
+		// ),
+	);
+
+	$config = array(
+		'id'           => 'stirtingale',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
+/* ========================================================================================================================
+
 ACF Options
 
 ======================================================================================================================== */
